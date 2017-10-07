e8a0608defd13bcd874578ec073ae2153581c6b3
Sam Schneller <sschnell@oswego.edu>
2017-10-07 14:24:49 -0400

    Adding basic build stuff for bot

--- src/main/java/TabletopBot.java
+++ src/main/java/TabletopBot.java
@@ -1,2 +1,52 @@
+import com.jagrosh.jdautilities.commandclient.CommandClientBuilder;
+import com.jagrosh.jdautilities.waiter.EventWaiter;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.OnlineStatus;
+import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
+
 public class TabletopBot {
+
+    public static void main(String[] args) throws IOException, LoginException, IllegalArgumentException, RateLimitedException {
+        String token = args[0];
+        String owner = args[1];
+
+        // define an eventwaiter, dont forget to add this to the JDABuilder!
+        EventWaiter waiter = new EventWaiter();
+
+        // define a command client
+        CommandClientBuilder client = new CommandClientBuilder();
+
+        // The default is "Type !!help" (or whatver prefix you set)
+        client.useDefaultGame();
+
+        // sets the owner of the bot
+        client.setOwnerId(owner);
+
+        // sets emojis used throughout the bot on successes, warnings, and failures
+        client.setEmojis("\uD83D\uDE03", "\uD83D\uDE2E", "\uD83D\uDE26");
+
+        // sets the bot prefix
+        client.setPrefix("!");
+
+        // start getting a bot account set up
+        new JDABuilder(AccountType.BOT)
+                // set the token
+                .setToken(token)
+
+                // set the game for when the bot is loading
+                .setStatus(OnlineStatus.DO_NOT_DISTURB)
+                .setGame(Game.of("loading..."))
+
+                // add the listeners
+                .addEventListener(waiter)
+                .addEventListener(client.build())
+
+                // start it up!
+                .buildAsync();
+    }
 }
