b014fb899009970e1fa44c981128ff168620dcc0
Calvin <clawrenc@oswego.edu>
2017-10-07 15:32:50 -0400

    figured out bot, creating commands

--- src/main/java/TabletopBot.java
+++ src/main/java/TabletopBot.java
@@ -1,2 +1,79 @@
-public class TabletopBot {
+import com.jagrosh.jdautilities.commandclient.CommandClientBuilder;
+import com.jagrosh.jdautilities.commandclient.examples.AboutCommand;
+import com.jagrosh.jdautilities.commandclient.examples.PingCommand;
+import com.jagrosh.jdautilities.commandclient.examples.ShutdownCommand;
+import com.jagrosh.jdautilities.waiter.EventWaiter;
+import net.dv8tion.jda.core.*;
+import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class TabletopBot{
+
+    public static void main(String[] args) throws IOException, LoginException, IllegalArgumentException, RateLimitedException
+    {
+
+        // the first is the bot token
+        String token = args[0];
+
+        // the second is the bot's owner's id
+        String ownerId = args[1];
+
+        // define an eventwaiter, dont forget to add this to the JDABuilder!
+        EventWaiter waiter = new EventWaiter();
+
+        // define a command client
+        CommandClientBuilder client = new CommandClientBuilder();
+
+        // The default is "Type !!help" (or whatver prefix you set)
+        client.useDefaultGame();
+
+        // sets the owner of the bot
+        client.setOwnerId(ownerId);
+
+        // sets emojis used throughout the bot on successes, warnings, and failures
+        client.setEmojis("\uD83D\uDE03", "\uD83D\uDE2E", "\uD83D\uDE26");
+
+        // sets the bot prefix
+        client.setPrefix("!");
+
+        // adds commands
+        client.addCommands(
+                // command to show information about the bot
+                new AboutCommand(Color.BLUE, "an example bot",
+                        new String[]{"Cool commands","Nice examples","Lots of fun!"},
+                        new Permission[]{Permission.ADMINISTRATOR}),
+
+                // command to check bot latency
+                new PingCommand(),
+
+                // command to shut off the bot
+                new ShutdownCommand());
+
+        // start getting a bot account set up
+        new JDABuilder(AccountType.BOT)
+                // set the token
+                .setToken(token)
+
+                // set the game for when the bot is loading
+                .setStatus(OnlineStatus.DO_NOT_DISTURB)
+                .setGame(Game.of("loading..."))
+
+                // add the listeners
+                .addEventListener(waiter)
+                .addEventListener(client.build())
+
+                // start it up!
+                .buildAsync();
+    }
+
+
 }
